a6b89ae91d755ee97fb4d8356cbc453d97617c51
dongji1111 <33479512+dongji1111@users.noreply.github.com>
2017-12-13 17:47:06 +0800

    1

--- src/com/github/shiftac/upartier/serverdata/Fetch.java
+++ src/com/github/shiftac/upartier/serverdata/Fetch.java
@@ -418,7 +418,7 @@ public class Fetch {
 			while(rs.next()) {
 			int mypostcount =rs.getInt("PostCount");
 			mypostcount=mypostcount+1;
-		    sql="update block set PostCount=? where UserId=?";
+		    sql="update user set PostCount=? where UserId=?";
 			stmt=conn.prepareStatement(sql);
 			stmt.setInt(1, mypostcount);
 			stmt.setInt(2, post.userID);
